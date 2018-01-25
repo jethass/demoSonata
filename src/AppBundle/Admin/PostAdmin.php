2cf5b30a201f0778a1ddfbeb9bd40357448fbd16
jethass <jethass@hotmail.com>
2018-01-25 17:02:37 +0100

    project sonata admin

---
+++ src/AppBundle/Admin/PostAdmin.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class PostAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('title', 'text')->add('body', 'textarea');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('title');
+    }
+}
